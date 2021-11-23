7a93e7e31ece21101d09b2055c6607658858a865
Nikerlas <nicholas.ernesto23@gmail.com>
2021-11-23 23:00:12 +0700

    Initial commit

---
+++ app/src/main/java/com/smk9smg/dashboard/MainActivity.java
@@ -0,0 +1,13 @@
+package com.smk9smg.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
